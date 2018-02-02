18766628879e11b05b1efa421ab0eebc24c3fdc5
vedabrat <vedabrat>
2018-02-02 13:01:26 -0500

    Welcome controller created

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def howto
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #howto" do
+    it "returns http success" do
+      get :howto
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
