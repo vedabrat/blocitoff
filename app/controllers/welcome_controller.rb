b9db504952ec04fab4a0b7e9268761973f16ef45
vedabrat <vedabrat>
2018-02-14 18:53:49 -0500

    this doesnt feel like it looks like a user profile, but i did everything the checkpoint asked me to do...

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 before_action :authenticate_user!
   def index
+    if user_signed_in?
+      redirect_to users_show_path(current_user)
+    end
   end
 
   def howto
