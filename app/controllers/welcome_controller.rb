43a3e9566d0ba91bff6aae282eef4190e7a0c9d3
vedabrat <vedabrat>
2018-03-13 17:54:30 -0400

    getting us back on track

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    if user_signed_in?
-      redirect_to users_show_path(current_user)
-    end
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   devise_for :users
-  get 'welcome/show'
-  get 'users/show'
+  get 'welcome/index'
 
   get 'welcome/about'
   root to: 'welcome#index'
