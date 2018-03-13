c9bb44e1561cacabe1cd05a9d71080ea4a06e7d4
vedabrat <vedabrat>
2018-03-13 19:58:40 -0400

    profile created, show metchod made, routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
 
+  devise_for :users
+  get 'welcome/profile'
+  get 'welcome/index'
   get 'welcome/about'
-  root to: 'welcome#index'
+  get 'users/show'
+  resources :welcome
+  resources :users
+  root to: 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
