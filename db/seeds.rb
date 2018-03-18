6dd0bebcf8e8a562529e3bcecf4c62a1a0e2b70d
vedabrat <vedabrat>
2018-03-17 20:11:12 -0400

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+    Item.new(name: Faker::Lorem.sentence)
+    User.new(id: (User.count + 1), email: (Faker::Lorem.word+"@gmail.com"))
+end
+
+puts "Seed finished"
+puts "#{Item.count} items created"
+puts "#{User.count} users created"
